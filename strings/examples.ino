f7d8fcc90a53f9975be6e653787560d1da953a8c
geronimoooooooo <geronimoooooooo@gmail.com>
2019-03-06 13:01:52 +0100

    Create examples.ino

---
+++ strings/examples.ino
@@ -0,0 +1,23 @@
+char myStr[6];
+myStr[0] = 'A';
+char myStr[] = "Hello";
+
+myStr[5] = 0; //null terminated
+
+num = strlen(str);
+strcpy(out_str, str); //wenn man String out_str neuen Text geben möchte
+
+// (5) add a string to the end of a string (append)
+   strcat(out_str, " sketch.");
+   
+  String str = "some String";
+  char arr[] = "This is an array of chars";
+  char arr2[] = "This 2nd array";
+  char *arr3 = "arr3";
+  Serial.println(arr);
+  Serial.println(arr2);  
+  strcpy(arr2, arr);
+  Serial.println(arr2);  
+  Serial.println(arr3);  
+  strcpy(arr2, arr3);
+  Serial.println(arr2);  
