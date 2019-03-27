119c7a7728cd361da7467d94f53738d16b62037c
geronimoooooooo <geronimoooooooo@gmail.com>
2019-03-27 16:39:18 +0100

    Update lcd.ino

--- lcd.ino
+++ lcd.ino
@@ -1,4 +1,22 @@
 //snippets about M5Stack Lcd
+//lcd x_width=320, y_height=240
 
-  
-  M5.begin(); // initialize the M5Stack object
+M5.begin(); // initialize the M5Stack object
+
+//display
+//start top left corner, x to right, y down
+M5.Lcd.fillScreen(BLACK); // Lcd display RED, GREEN, BLUE, BLACK, WHITE
+
+//text
+M5.Lcd.setCursor(10, 10);
+M5.Lcd.setTextColor(WHITE);
+M5.Lcd.setTextSize(1);
+M5.Lcd.printf("Display Test!");
+
+//graphics
+M5.Lcd.drawRect(100, 100, 50, 50, BLUE);
+M5.Lcd.fillRect(100, 100, 50, 50, BLUE);
+M5.Lcd.drawCircle(100, 100, 50, RED);
+M5.Lcd.fillCircle(100, 100, 50, RED);
+M5.Lcd.drawTriangle(30, 30, 180, 100, 80, 150, YELLOW);
+M5.Lcd.fillTriangle(30, 30, 180, 100, 80, 150, YELLOW);
