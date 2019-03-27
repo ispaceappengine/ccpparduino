0c157daae0ba9b4e18800f5bdb04ec251677e690
geronimoooooooo <geronimoooooooo@gmail.com>
2019-03-27 16:53:29 +0100

    Update power.ino

--- power.ino
+++ power.ino
@@ -1,4 +1,7 @@
 #include <M5Stack.h>
 
+One pressing will only reset the ESP32, double pressing will be treated as power off, so the PMU will directly cut-off the MCU's power.
+ 
 M5.setWakeupButton(BUTTON_A_PIN); //press 1.button and m5stack wakes up
 M5.powerOFF();  // puts the whole machine to sleep
+esp_deep_sleep_start(); //putting the esp32 to sleep 
