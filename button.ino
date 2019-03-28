4867fbb0d638e17197f6bf67993b6260dfb69a10
geronimoooooooo <geronimoooooooo@gmail.com>
2019-03-28 13:47:58 +0100

    Update button.ino

--- button.ino
+++ button.ino
@@ -1,4 +1,14 @@
 
+ M5.BtnA/B/C.read();
+ M5.BtnA/B/C.isPressed();
+ M5.BtnA/B/C.isReleased();
+ M5.BtnA/B/C.wasPressed();
+ M5.BtnA/B/C.wasReleased();
+ M5.BtnA/B/C.wasreleasedFor()
+ M5.BtnA/B/C.pressedFor(uint32_t ms);
+ M5.BtnA/B/C.releasedFor(uint32_t ms);
+ M5.BtnA/B/C.lastChange();
+
  if(M5.BtnA.wasPressed()) {
  if(M5.BtnB.wasPressed())
  
