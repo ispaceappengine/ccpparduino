eebccfb8e22d4e7ebbbb00da2a3f6255e466dd44
geronimoooooooo <geronimoooooooo@gmail.com>
2019-04-02 09:36:15 +0200

    Update WiFi.ino

--- WiFi.ino
+++ WiFi.ino
@@ -1,2 +1,4 @@
-Example für wifi connect und Ausgabe MAC, IP, etc.. https://www.arduino.cc/en/Guide/ArduinoWiFiShield#toc1
+Example für wifi connect und Ausgabe MAC, IP, etc.. 
+https://www.arduino.cc/en/Guide/ArduinoWiFiShield#toc1
+https://www.arduino.cc/en/Tutorial/WiFiNINAScanNetworksAdvanced
 Send GET to Internet, list WiFi: https://www.arduino.cc/en/Tutorial/WiFiWebClient
