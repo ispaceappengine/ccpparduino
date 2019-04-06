21e07ebddaef245500f517c2a66908ee7ce2f08e
geronimoooooooo <geronimoooooooo@gmail.com>
2019-04-06 20:48:02 +0200

    Update ble.cpp

--- ble.cpp
+++ ble.cpp
@@ -1,2 +1,3 @@
 Adding Bluetooth LE features (von PlatformIO)
 https://docs.platformio.org/en/latest/tutorials/espressif32/arduino_debugging_unit_testing.html#adding-bluetooth-le-features
+https://docs.platformio.org/en/latest/tutorials/nordicnrf52/arduino_debugging_unit_testing.html#adding-bluetooth-le-features
