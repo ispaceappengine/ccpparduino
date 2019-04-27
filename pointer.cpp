006a5134c960c3a61e3c16280a0928528c9dc5c3
geronimoooooooo <geronimoooooooo@gmail.com>
2019-04-27 21:23:55 +0200

    Update pointer.cpp

--- pointer.cpp
+++ pointer.cpp
@@ -11,6 +11,37 @@ void printPointer(const char *str){
 hello("hello there");
 void hello(const char *str) { Serial.println(str); }
 ################################################################
+  int zahl = 100;
+  printf ("Value von zahl: %d\n", zahl);	//100
+  int *p1 = &zahl;
+  //100, * holt den Wert, der sich in der Speicheradresse befindet
+  //die in p1 drin ist
+  printf ("Value von *p1: %d\n", *p1);
+  printf ("Value von p1: %d\n", p1);	//Speicheradresse: 1822935687
+
+  int *p2;
+  p2 = &zahl;
+  printf ("Value von *p2: %d\n", *p2);	//100
+  printf ("Value von p2: %d\n", p2);	//Speicheradresse: 1822935687
+
+  int zahl2 = 33;
+  //p2 = zahl2; //ERROR invalid conversion from int to int*
+  p2 = &zahl2;
+  printf ("Value von *p2: %d\n", *p2);	//33
+  printf ("Value von p2: %d\n", p2);	//Speicheradresse: 35939284973
+
+  int a = 100, *pa = &a;	/* a wird mit 100 initialisiert, pa zeigt gleich auf a */
+  printf ("a = %d\n", a);	//100
+  printf ("*pa = %d\n", *pa);	//100
+
+  *pa = 333;			//write new value to variable a (Speicheradresse von a)
+  printf ("a = %d\n", a);	//333
+  printf ("*pa = %d\n", *pa);	//333
+
+int feld[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, i2;
+ 
+  for (i2 = 0; i2 < 10; i2++) 
+    printf ("Element %d = %d\n", i2, *(feld + i2) );
 ################################################################
 ################################################################
 ################################################################
