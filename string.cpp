ca6c3dc05997c14a7e54f8e95ed221c3309c388c
geronimoooooooo <geronimoooooooo@gmail.com>
2019-04-28 18:32:42 +0200

    Create string.cpp

---
+++ string.cpp
@@ -0,0 +1,8 @@
+2 types of strings in C++
++ Strings that are objects of string class (The Standard C++ Library string class)
++ C-strings (C-style Strings) (Array of type char)
+
+#####################################################
+char str[] = "C++"; // "C++" has 3 character, the null character \0 is added to the end of the string automatically.
+char str[4] = "C++";
+char str[100] = "C++"; //not needed to use up all the space
