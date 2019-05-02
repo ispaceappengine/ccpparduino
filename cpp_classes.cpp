c9d53a3b830debb1a835dd9a9aeddbcc0d231be3
geronimoooooooo <geronimoooooooo@gmail.com>
2019-05-02 12:35:50 +0200

    Create cpp_classes.cpp

---
+++ cpp_classes.cpp
@@ -0,0 +1,7 @@
+https://www.geeksforgeeks.org/c-classes-and-objects/
+Class: building block of c++, user defined data type. Blueprint for creating objects/instances. 
+Class has own data members and functions.
+Memory wird erst dann allokiert, wenn das Objekt initialisiert.
+
+Access modifier: private, public, protected
+Access data members and functions with "." obj.printName()
