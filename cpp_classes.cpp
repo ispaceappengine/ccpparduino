17f6c552ddaef392dddb9722266fe3fdd1e057fe
geronimoooooooo <geronimoooooooo@gmail.com>
2019-05-03 02:48:48 +0200

    Update cpp_classes.cpp

--- cpp_classes.cpp
+++ cpp_classes.cpp
@@ -1,3 +1,6 @@
+http://www.java2s.com/Tutorial/Cpp/0180__Class/Catalog0180__Class.htm
+Paradebeispiel: http://www.java2s.com/Tutorial/Cpp/0180__Class/Anarrayofpointerstoobjects.htm
+#################################################################
 https://www.geeksforgeeks.org/c-classes-and-objects/
 Class: building block of c++, user defined data type. Blueprint for creating objects/instances. 
 Class has own data members and functions.
@@ -5,3 +8,4 @@ Memory wird erst dann allokiert, wenn das Objekt initialisiert.
 
 Access modifier: private, public, protected
 Access data members and functions with "." obj.printName()
+######################################################################
