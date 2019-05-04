f8a95882e22693b445c6aba8967899894080a91e
geronimoooooooo <geronimoooooooo@gmail.com>
2019-05-05 01:50:50 +0200

    Create struct.c

---
+++ struct.c
@@ -0,0 +1,12 @@
+A structure in C is a collection of variables of different data types under a single name. WIE KLASSE IN OOP SPRACHEN.
+
+struct Person
+{
+    char name[50];
+    int citNo;
+    float salary;
+};
+
+create: struct Person person1, person2, p[20];
+
+Accessing members of struct mit (.) oder pointer operator (->) person1.salery
