d66c784721294291766cf49eab40e5e55423e4ce
geronimoooooooo <geronimoooooooo@gmail.com>
2019-05-05 01:55:15 +0200

    Update struct.c

--- struct.c
+++ struct.c
@@ -5,8 +5,7 @@ struct Person
     char name[50];
     int citNo;
     float salary;
-};
+}; // person1, person2
 
 create: struct Person person1, person2, p[20];
-
 Accessing members of struct mit (.) oder pointer operator (->) person1.salery
