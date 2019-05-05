0abbb35b252f9c84643d7359dcfa2ebfab7ac4a9
geronimoooooooo <geronimoooooooo@gmail.com>
2019-05-05 03:58:47 +0200

    Update struct.c

--- struct.c
+++ struct.c
@@ -1,8 +1,5 @@
 A structure in C is a collection of variables of different data types under a single name. WIE KLASSE IN OOP SPRACHEN.
 
-
-
-
 struct Person
 {
     char name[50];
@@ -20,4 +17,6 @@ get: person1.age  per->age
 // address of num1 and num2 is passed
 swap( &num1, &num2);
 void swap(int* n1, int* n2)
-######################
+###########################################
+Union wie struct, nur kann Union nur ein einzelnes member gleichzeitig abfragen(wert langfristig halten) und die größe von union ist nur so groß wie das größte member.
+###########################################
