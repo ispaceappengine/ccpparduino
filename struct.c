adae1bb25cfc57742dff3de86ebdf8d769a2558f
geronimoooooooo <geronimoooooooo@gmail.com>
2019-05-05 03:47:09 +0200

    Update struct.c

--- struct.c
+++ struct.c
@@ -1,11 +1,23 @@
 A structure in C is a collection of variables of different data types under a single name. WIE KLASSE IN OOP SPRACHEN.
 
+
+
+
 struct Person
 {
     char name[50];
     int citNo;
+    int age;
     float salary;
 }; // person1, person2
 
-create: struct Person person1, person2, p[20];
+create: struct Person person1, person2, p[20], *per;
 Accessing members of struct mit (.) oder pointer operator (->) person1.salery
+per = &person1;
+set: &person1.age &per->age
+get: person1.age  per->age
+
+// address of num1 and num2 is passed
+swap( &num1, &num2);
+void swap(int* n1, int* n2)
+######################
