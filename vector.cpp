4326ebaf7f2a76fa7dcdab4f0bc12285b27b5cc5
geronimoooooooo <geronimoooooooo@gmail.com>
2019-05-07 01:30:12 +0200

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -1,5 +1,8 @@
+EXAMPLES: https://thispointer.com//how-to-use-vector-efficiently/
+
 Sorting Vector of Pairs in C++ | Set 1 (Sort by first and second): https://www.geeksforgeeks.org/sorting-vector-of-pairs-in-c-set-1-sort-by-first-and-second/
 Adding to a vector of pair: https://stackoverflow.com/questions/7897050/adding-to-a-vector-of-pair
+https://thispointer.com//how-to-use-vector-efficiently/
 ################################################################
 ################################################################
 ################################################################
