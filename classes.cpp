15ac518fe2a5579cdd523406462e51f3239bfda0
geronimoooooooo <geronimoooooooo@gmail.com>
2019-05-06 15:49:13 +0200

    Update and rename cpp_classes.cpp to classes.cpp

--- classes.cpp
+++ classes.cpp
@@ -1,11 +1,17 @@
 http://www.java2s.com/Tutorial/Cpp/0180__Class/Catalog0180__Class.htm
 Paradebeispiel: http://www.java2s.com/Tutorial/Cpp/0180__Class/Anarrayofpointerstoobjects.htm
 #################################################################
+
 https://www.geeksforgeeks.org/c-classes-and-objects/
+http://www.trytoprogram.com/cplusplus-programming/class-object/
+
 Class: building block of c++, user defined data type. Blueprint for creating objects/instances. 
-Class has own data members and functions.
+Object is an instance of a Class. Class has own data members and functions.
 Memory wird erst dann allokiert, wenn das Objekt initialisiert.
 
 Access modifier: private, public, protected
 Access data members and functions with "." obj.printName()
 ######################################################################
+define outside of class: scope resolution :: operator along with class name and function name
+void Geeks::printname() 
+{
