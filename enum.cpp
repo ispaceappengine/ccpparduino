a88be632407a24c4961208ed30913dbe5d317cad
geronimoooooooo <geronimoooooooo@gmail.com>
2019-05-07 23:53:49 +0200

    Create enum.cpp

---
+++ enum.cpp
@@ -0,0 +1,8 @@
+https://www.javatpoint.com/cpp-enumeration
+
+Enum in C++ is a data type that contains fixed set of constants. It can be used for days of the week (SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY and SATURDAY). The C++ enum constants are static and final implicitly.
+###############################################################
+enum week { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday };  
+
+week day;
+day = Friday;
