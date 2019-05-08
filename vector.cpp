2c58788da9ba30a8677580d293fffeef1b79fdad
geronimoooooooo <geronimoooooooo@gmail.com>
2019-05-08 21:24:16 +0200

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -1,11 +1,18 @@
-EXAMPLES: https://thispointer.com//how-to-use-vector-efficiently/
-
-Sorting Vector of Pairs in C++ | Set 1 (Sort by first and second): https://www.geeksforgeeks.org/sorting-vector-of-pairs-in-c-set-1-sort-by-first-and-second/
-Adding to a vector of pair: https://stackoverflow.com/questions/7897050/adding-to-a-vector-of-pair
++ Auflistung aller Functions von einem Vector: http://www.cplusplus.com/reference/vector/vector/
++ EXAMPLES: https://thispointer.com//how-to-use-vector-efficiently/
++ Sorting Vector of Pairs in C++ | Set 1 (Sort by first and second): https://www.geeksforgeeks.org/sorting-vector-of-pairs-in-c-set-1-sort-by-first-and-second/
++ Adding to a vector of pair: https://stackoverflow.com/questions/7897050/adding-to-a-vector-of-pair
 https://thispointer.com//how-to-use-vector-efficiently/
 ################################################################
+Unlike arrays, vectors are dynamically sized
 ################################################################
+vector<dataType> vectorName;
+vector<dataType> vectorName(size);//if you want to provide a size
+vectorName.push_back(value);//adding an item to a vector
+vectorName[index]; //accessing an index of a vector
+vectorName[index] = value; //changing a value at a certain index
 ################################################################
+
 ################################################################
 ################################################################
 ################################################################
