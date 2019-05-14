e8ae922a8d79ed160037eb1e2b4810e48b3b10c5
geronimoooooooo <geronimoooooooo@gmail.com>
2019-05-14 17:40:34 +0200

    Update pair.cpp

--- pair.cpp
+++ pair.cpp
@@ -1,10 +1,13 @@
 GUTE ERKLÄRUNG MIT EXAMPLES: https://www.geeksforgeeks.org/pair-in-cpp-stl/
 
-
-
 ############################
 + pair ist ein container, defined im <utility> header
 + besteht aus 2 elementen. first und second. allows to combine 2 different types. Map, Hash_Map nutzen auch pair.
+####################################    
+Tuple: It is an object capable to hold a collection of elements. Each element can be of a different type.
+Pair object muss 2 Elemente haben, Tuple ist es egal.
+std::get<0>(tupleName)
+#####################################
 
 #include <utility> 
     pair <int, char> PAIR1 ;     
