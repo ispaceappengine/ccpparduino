028687f0debe66a336e948ed9f093dd9fd4adb76
geronimoooooooo <geronimoooooooo@gmail.com>
2019-06-12 23:42:43 +0200

    Update mac_wifi.h

--- mac_wifi.h
+++ mac_wifi.h
@@ -1,1 +1,6 @@
 ESP32 Arduino Tutorial: Obtaining MAC address of stations connected to soft AP: https://www.dfrobot.com/blog-1024.html
+http://arduino-er.blogspot.com/2017/04/get-wifi-mac-address-of-esp32-module.html
+
+esp_wifi_get_mac: https://docs.espressif.com/projects/esp-idf/en/latest/api-reference/network/esp_wifi.html
+
+[REQ] ESP.getEfuseMac() to String: https://www.reddit.com/r/esp32/comments/7soaoi/req_espgetefusemac_to_string/
