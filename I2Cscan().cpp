abda361cb74ae1d163045e4046c7ea4f5195d2ec
geronimoooooooo <geronimoooooooo@gmail.com>
2019-08-15 00:03:08 +0200

    Update I2Cscan().cpp

--- I2Cscan().cpp
+++ I2Cscan().cpp
@@ -1,3 +1,62 @@
+https://playground.arduino.cc/Main/I2cScanner/
+
+#include <Arduino.h>
+#include <M5Stack.h>
+#include <Wire.h>
+
+
+void setup()
+{
+  Wire.begin();
+
+  Serial.begin(115200);//9600
+  while (!Serial);             // Leonardo: wait for serial monitor
+  Serial.println("\nI2C Scanner");
+}
+
+
+void loop()
+{
+  byte error, address;
+  int nDevices;
+
+  Serial.println("Scanning...");
+
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) 
+  {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if (error == 0)
+    {
+      Serial.print("I2C device found at address 0x");
+      if (address<16) 
+        Serial.print("0");
+      Serial.print(address,HEX);
+      Serial.println("  !");
+
+      nDevices++;
+    }
+    else if (error==4) 
+    {
+      Serial.print("Unknown error at address 0x");
+      if (address<16) 
+        Serial.print("0");
+      Serial.println(address,HEX);
+    }    
+  }
+  if (nDevices == 0)
+    Serial.println("No I2C devices found\n");
+  else
+    Serial.println("done\n");
+
+  delay(4000);           // wait 4 seconds for next scan
+}
+##################################################################
 void I2Cscan()
 {
 // scan for i2c devices
